d8b7e91111ee2c79e20995f36f845e4feca15152
Matthew Tebbs <matthew@muddytummy.com>
2018-06-08 08:47:18 -0700

    Moving test cases to proper utility.

--- test/app.ts
+++ test/app.ts
@@ -6,17 +6,6 @@
 
 import * as PxtCloud from '..';
 
-const debug = require('debug')('pxt-cloud:test');
+const debug = require('debug')('pxt-cloud:app');
 
-function testWorldAPI(api: PxtCloud.WorldAPI) {
-    api.on('user joined', debug);
-    api.on('user left', debug);
-
-    api.addUser({ name: 'Bobby Joe' }, debug);
-    api.addUser({ name: 'Bobby Joe' }, debug);
-
-    api.removeUser(debug);
-    api.removeUser(debug);
-}
-
-PxtCloud.startServer().then(server => testWorldAPI(server.worldAPI!)).catch(debug);
+PxtCloud.startServer().catch(debug);
