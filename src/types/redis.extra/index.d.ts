9a94414d40086cc3444ab041fad2ce2a826040d4
Matthew Tebbs <matthew@muddytummy.com>
2018-06-20 16:14:45 -0700

    Add stream commands to @types/redis and use beta version of redis-commands.

---
+++ src/types/redis.extra/index.d.ts
@@ -0,0 +1,41 @@
+/*
+    MIT License
+
+    Copyright (c) 2018 MuddyTummy Software LLC
+*/
+
+import * as _ from 'redis';
+
+declare module 'redis' {
+    export type StreamID = string | '-' | '+';
+
+    export interface Commands<R> {
+        /***
+         * Appends the specified stream entry to the stream at the specified key.
+         */
+        xadd: OverloadedSetCommand<string | number, boolean, R>;
+        XADD: OverloadedSetCommand<string | number, boolean, R>;
+
+        /**
+         * Returns the number of entries inside a stream.
+         */
+        xlen(key: string, cb?: Callback<number>): R;
+        XLEN(key: string, cb?: Callback<number>): R;
+
+        /**
+         * Returns the stream entries matching a given range of IDs.
+         */
+        xrange(key: string, start: StreamID, stop: StreamID, cb?: Callback<string[]>): R;
+        XRANGE(key: string, start: StreamID, stop: StreamID, cb?: Callback<string[]>): R;
+        xrange(key: string, start: StreamID, stop: StreamID, count: string, limit: number, cb?: Callback<string[]>): R;
+        XRANGE(key: string, start: StreamID, stop: StreamID, count: string, limit: number, cb?: Callback<string[]>): R;
+    
+        /**
+         * Returns in REVERSE order the stream entries matching a given range of IDs.
+         */
+        xrevrange(key: string, stop: StreamID, start: StreamID, cb?: Callback<string[]>): R;
+        XREVRANGE(key: string, stop: StreamID, start: StreamID, cb?: Callback<string[]>): R;
+        xrevrange(key: string, stop: StreamID, start: StreamID, count: string, limit: number, cb?: Callback<string[]>): R;
+        XREVRANGE(key: string, stop: StreamID, start: StreamID, count: string, limit: number, cb?: Callback<string[]>): R;
+    }
+}
